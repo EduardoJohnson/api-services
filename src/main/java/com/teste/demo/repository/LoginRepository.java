8786f515d5750a28c851329f557b1a6762e1ca6a
eduardojohnson <eduardocarvalhodb@gmail.com>
2022-07-06 17:47:57 -0300

    Create class LoginRepository

---
+++ src/main/java/com/teste/demo/repository/LoginRepository.java
@@ -0,0 +1,9 @@
+package com.teste.demo.repository;
+
+import com.teste.demo.entity.LoginEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoginRepository extends JpaRepository<LoginEntity, Long> {
+}
