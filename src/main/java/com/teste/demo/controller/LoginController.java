54e2128dbdb2e047c4bdfea775f5f011589579c6
eduardojohnson <eduardocarvalhodb@gmail.com>
2022-07-08 21:59:54 -0300

    Create class LoginController

---
+++ src/main/java/com/teste/demo/controller/LoginController.java
@@ -0,0 +1,28 @@
+package com.teste.demo.controller;
+
+
+import com.teste.demo.entity.LoginEntity;
+import com.teste.demo.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+    @Autowired
+    private LoginService loginService;
+
+
+    @GetMapping("/get")
+    public List<LoginEntity> findAll() {
+        return loginService.listUser();
+    }
+
+    @PostMapping("/cadastro")
+    public String create(@RequestBody LoginEntity loginEntity){
+        return loginService.saveLogin(loginEntity);
+    }
+}
