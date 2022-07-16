e8617e9218b3c7417c05a73c6a75a63d2f6143c4
eduardojohnson <eduardocarvalhodb@gmail.com>
2022-07-16 13:35:56 -0300

    jpa repository correction

--- src/main/java/com/teste/demo/repository/PostRepository.java
+++ src/main/java/com/teste/demo/repository/PostRepository.java
@@ -3,7 +3,6 @@ package com.teste.demo.repository;
 
 import com.teste.demo.entity.PostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 @Repository
