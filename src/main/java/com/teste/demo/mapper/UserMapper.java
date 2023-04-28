932c5e730ebc140715d28933aedecfcead7ec1b7
eduardojohnson <eduardocarvalhodb@gmail.com>
2023-04-27 21:50:59 -0300

    config mapper

--- src/main/java/com/teste/demo/mapper/UserMapper.java
+++ src/main/java/com/teste/demo/mapper/UserMapper.java
@@ -2,7 +2,7 @@ package com.teste.demo.mapper;
 
 
 import com.teste.demo.dro.UserDto;
-import com.teste.demo.entity.TesteEntity;
+import com.teste.demo.entity.PeopleEntity;
 import ma.glasnost.orika.MapperFacade;
 import ma.glasnost.orika.MapperFactory;
 import ma.glasnost.orika.impl.DefaultMapperFactory;
@@ -11,32 +11,32 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserMapper {
 
-        private MapperFactory mapperFactory;
-        private MapperFacade mapperFacade;
+    private MapperFactory mapperFactory;
+    private MapperFacade mapperFacade;
 
-        public UserMapper(){
+    public UserMapper(){
 
-            mapperFactory = new DefaultMapperFactory.Builder().build();
-            mapperFactory.classMap(TesteEntity.class, UserDto.class)
-                    //2 campos para mapear
-                    // email e name
-                    .field("name", "nome")
-                    .field("email","emailPessoa")
-                    .byDefault()
-                    .register();
-            mapperFacade = mapperFactory.getMapperFacade();
+        mapperFactory = new DefaultMapperFactory.Builder().build();
+        mapperFactory.classMap(PeopleEntity.class, UserDto.class)
+                //2 campos para mapear
+                // email e name
+                .field("name", "fistName")
+                .field("email","emailPerson")
+                .byDefault()
+                .register();
+        mapperFacade = mapperFactory.getMapperFacade();
 
-        }
+    }
 
 
-        public TesteEntity dtoParaEntity(UserDto userDto){
+    public PeopleEntity dtoParaEntity(UserDto userDto){
 
-            return mapperFacade.map(userDto, TesteEntity.class);
+        return mapperFacade.map(userDto, PeopleEntity.class);
 
-        }
+    }
 
-            public UserDto entityParaDto(TesteEntity testeEntity){
-             return mapperFacade.map(testeEntity, UserDto.class);
-            }
+    public UserDto entityParaDto(PeopleEntity peopleEntity){
+        return mapperFacade.map(peopleEntity, UserDto.class);
+    }
 
 }
